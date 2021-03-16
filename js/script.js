e1bce6ed19e22dc20b36b21e5bf17c382d72818a
puchimilk <puchimilk1539@gmail.com>
2021-03-16 17:04:23 +0900

    master

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 $(function () {
-    if (window.matchMedia('(max-width: 800px)').matches) {
-        $('.btn-label').on('click', function () {
-            $('.menu-content').slideToggle();
-            $('.menu-content').toggleClass('active');
-        });
-        $('a[href^="#"]').on('click', function () {
-            $('.menu-content').slideToggle();
-            $('.menu-content').removeClass('active');
-            $('#btn-toggle').prop('checked', false);
-            console.log('close');
-        });
-    }
-    $('a[href^="#"]').on('click', function () {
-        var header = $('header').outerHeight();
-        var speed = 500;
-        var href = $(this).attr('href');
-        var target = $(href == '#' || href == '' ? 'html' : href);
-        var position = target.offset().top - header;
-        $('html, body').animate({ scrollTop: position }, speed, 'swing');
-        return false;
+  if (window.matchMedia('(max-width: 800px)').matches) {
+    $('.btn-label').on('click', function () {
+      $('.menu-content').slideToggle();
+      $('.menu-content').toggleClass('active');
     });
-    deSVG('.svg', true);
+    
+    $('a[href^="#"]').on('click', function () {
+      $('.menu-content').slideToggle();
+      $('.menu-content').removeClass('active');
+      $('#btn-toggle').prop('checked', false);
+    });
+  }
+  
+  $('a[href^="#"]').on('click', function () {
+    var header = $('header').outerHeight();
+    var speed = 500;
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top - header;
+    $('html, body').animate({ scrollTop: position }, speed, 'swing');
+      return false;
+  });
+  
+  deSVG('.svg', true);
 });
