84cee59da5280a4c91c6c44fcd593da6b10dc28a
puchimilk <puchimilk1539@gmail.com>
2023-02-11 17:10:30 +0900

    master

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-$(function () {  
-  $('a[href^="#"]').on('click', function () {
-    var header = $('header').outerHeight();
-    var speed = 500;
-    var href = $(this).attr('href');
-    var target = $(href == '#' || href == '' ? 'html' : href);
-    var position = target.offset().top - header;
-    $('html, body').animate({ scrollTop: position }, speed, 'swing');
-      return false;
+$(() => {
+  $('a[href^="#"]').on('click', () => {
+    const header = $('header').outerHeight();
+    const speed = 500;
+    const href = $(this).attr('href');
+    const target = $(href === '#' || href === '' ? 'html' : href);
+    const position = target.offset().top - header;
+
+    $('html, body').animate({ scrollTo: position }, speed, 'swing');
   });
 });
